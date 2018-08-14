90b43ebda2631983bc70e3ccc3d4dc4ff5ed13e1
Nariman Saftarli <nsaftarli@ryerson.ca>
2018-08-13 21:24:18 -0400

    Initial commit

--- CannyEdge.m
+++ CannyEdge.m
@@ -2,6 +2,8 @@
 
 fprintf('The test image:\n')
 img = imread('bowl-of-fruit.jpg');
+imshow(img, []);
+pause;
 img = rgb2gray(img);
 my_edges = MyCanny(img,3,10);
 imshow(my_edges,[]);
@@ -9,6 +11,8 @@ pause;
 
 fprintf('Second image:\n')
 img = imread('building.png');
+imshow(img, []);
+pause;
 img = rgb2gray(img);
 my_edges = MyCanny(img,3,15);
 imshow(my_edges,[]);
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+# Matlab Canny Edge Detection (Excluding Hysteresis Thresholding)
 
+## Examples
+
+
+<img src="bowl-of-fruit.jpg" width="300" height="300"> <img src="fruit-edges.png" width="300" height="300">
+
+<img src="building.png" width="300" height="300"> <img src="building-edges.png" width="300" height="300">
